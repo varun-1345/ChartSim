2ca59dcbbbdc29e460683ef1676ff40450ecc323
varun-1345 <76691042+varun-1345@users.noreply.github.com>
2023-09-14 21:05:36 +0530

    App.js

--- App.js
+++ App.js
@@ -5,53 +5,53 @@ function App() {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              <img
-                src="logo.jpg"
-                alt="Logo"
-                width="40"
-                height="35"
-                className="d-inline-block align-text-top"
-              />
-              Charts Simulator
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="/">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="/">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img
+              src="logo.jpg"
+              alt="Logo"
+              width="40"
+              height="35"
+              className="d-inline-block align-text-top"
+            />
+            Charts Simulator
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/">
+                  Contact
+                </a>
+              </li>
+            </ul>
           </div>
-        </nav>
-        <p className="type">
-          Select a particular chart from below dropdown below.
-        </p>
-      <div className="left-container"> 
+        </div>
+      </nav>
+      <p className="type">
+        Select a particular chart from below dropdown below.
+      </p>
+      <div className="left-container">
         <div className="selector">
           <b className="text-primary m-3">Type</b>
           <select className="form-select" aria-label="Default select example">
@@ -75,7 +75,6 @@ function App() {
           <br></br>
         </div>
 
-        
         <div className="tableclass">
           <table className="table table-bordered border-primary custom-table">
             <thead>
