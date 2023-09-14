371ac7ff85330dd83e024bf589287703715c7d4d
varun-1345 <76691042+varun-1345@users.noreply.github.com>
2023-09-14 21:05:09 +0530

    Rightcomp.js

--- Rightcomp.js
+++ Rightcomp.js
@@ -3,7 +3,11 @@ import "./App.css";
 const Rightcomp = () => {
   return (
         <div className="box">
-            <div className="box1 mx-5"></div>
+            <div className="box1 mx-5">
+                <h2>Hello World</h2>
+                <strong className="text-center text-decoration-underline"><i>Block is just a reference of where we will be displaying our charts</i></strong>
+                <img src="logo512.png" alt="logo"></img>
+            </div>
         </div>
   )
 }
